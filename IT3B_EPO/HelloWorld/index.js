fd941675719ab76b89964a70fb251b485ee5b2e1
Johanovský Karel <johanovsky@prumyslovkajihlava.cz>
2026-09-18 11:21:08 +0200

    20260918

--- IT3B_EPO/HelloWorld/index.js
+++ IT3B_EPO/HelloWorld/index.js
@@ -1,4 +1,20 @@
+if("serviceWorker" in navigator) {
+    window.addEventListener("load", ()=> {
+        navigator.serviceWorker.register("sw.js")
+        .then(registration => {
+            console.log("SW registered");
+        })
+        .catch(error => {
+            console.log("SW registration failed: ", error);
+        });
+    });
+}
+
 function sayHello() {
     //najdeme tag s id a zapiseme do jeho textu
-    document.getElementById("hello").innerText = "Hello world";
+    if(document.getElementById("hello").innerText == "Hello world!") {
+        document.getElementById("hello").innerText = "Ahoj svete!";
+    } else {
+        document.getElementById("hello").innerText = "Hello world!";
+    }
 }
